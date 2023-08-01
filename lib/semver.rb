0836c96ba8539aeb6854070be183665a797023a4
Daniel Beggan <dbeggan.external@jovoto.com>
2023-08-01 14:13:19 +0100

    Add initialize method and tests

--- lib/semver.rb
+++ lib/semver.rb
@@ -1,2 +1,11 @@
 class Semver
+  attr_accessor :version
+
+  def initialize(version)
+    if version.is_a?(String)
+      @version = version
+    else
+      raise ArgumentError, "Version must be a string."
+    end
+  end
 end
--- spec/semver_spec.rb
+++ spec/semver_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../lib/semver'
 
 describe Semver do
+  describe '.initialize' do
+    context "when a valid version is provided" do
+      it "should initialize without raising an error" do
+        expect { Semver.new("1.2.3") }.not_to raise_error
+      end
+    end
+
+    context "when an invalid version is provided" do
+      it "should raise an ArgumentError" do
+        expect { Semver.new(123) }.to raise_error(ArgumentError, "Version must be a string.")
+      end
+    end
+  end
 end
